e6c98197468a10791118158826c7124800484bb6
taylor dearden <taylordearden@gmail.com>
2019-09-17 16:13:18 -0600

    did some more

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,40 +1,42 @@
-const users = require("../models/users")
-let id = 1
+const users = require("../models/users");
+let id = 1;
 
 module.exports = {
-    register: (req, res) => {
-        const { session } = req
-        const { username, password } = req.body
+  register: (req, res) => {
+    const { session } = req;
+    const { username, password } = req.body;
 
-        user.push({ id, username, password })
-        id++
+    users.push({ id, username, password });
+    id++;
 
-        session.user.username = username
+    session.user.username = username;
 
-        res.status(200).send(session.user)
-    },
+    res.status(200).send(session.user);
+  },
 
-    login: (req, res) => {
-        const { session } = req
-        const { username, password } = req.body
+  login: (req, res) => {
+    const { session } = req;
+    const { username, password } = req.body;
 
-        const user = users.find(
-            user => user.username === username && user.password === password
-        );
+    const user = users.find(
+      user => user.username === username && user.password === password
+    );
 
-        if (user) {
-            session.user.username = user.username.status(200).send(session.user)
-        } else {
-            res.status(500).send("Unauthorized salad picking")
-        }
-    },
-
-    signout: (req, res) => {
-        req.session.destroy()
-        res.status(200).send(req.session)
-    },
-    getUser: (req, res) => {
-        const { session } = req;
-        res.status(200).send(session.user)
+    if (user) {
+      session.user.username = user.username;
+      res.status(200).send(session.user);
+    } else {
+      res.status(500).send("Unauthorized.");
     }
-}
+  },
+
+  signout: (req, res) => {
+    req.session.destroy();
+    res.status(200).send(req.session);
+  },
+
+  getUser: (req, res) => {
+    const { session } = req;
+    res.status(200).send(session.user);
+  }
+};
--- server/middlewares/checkForSession.js
+++ server/middlewares/checkForSession.js
@@ -1,9 +1,9 @@
 module.exports = function(req, res, next) {
-    const { session } = req
-
-    if(!session.use) {
-        session.use={ username: "", cart: [], total: 0}
+    const { session } = req;
+  
+    if (!session.user) {
+      session.user = { username: "", cart: [], total: 0 };
     }
-
-    next()
-}
+  
+    next();
+  };
